c79cf6639dfbc0d2d10f44f0db49de85f25186dc
Travis <tdelkreyes@gmail.com>
2023-07-11 12:44:41 -0400

    deleted update and end graphic lines

--- prettypictures.py
+++ prettypictures.py
@@ -1,12 +1,10 @@
 from gasp import *
-def draw()
-    begin_graphics()
-    Circle((310, 210), 100)
-    Circle((275, 230), 15)
-    Circle((345, 230), 15)
-    Arc((310, 210), 60, 225, 315)
-    Line((310, 230), (280, 180))
-    Line((290, 190), (320, 180))
+begin_graphics()
+Circle((310, 210), 100)
+Circle((275, 230), 15)
+Circle((345, 230), 15)
+Arc((310, 210), 60, 225, 315)
+Line((310, 230), (280, 180))
+Line((290, 190), (320, 180))
 
-update_when('key_pressed')
-end_graphics()
+
