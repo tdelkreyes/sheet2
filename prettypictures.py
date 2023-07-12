2a2499a9c5a20f07612c131270d7054388013c92
Travis <tdelkreyes@gmail.com>
2023-07-11 23:13:19 -0400

    added def draw_face to the bottom

--- prettypictures.py
+++ prettypictures.py
@@ -14,3 +14,12 @@ def moan():
     print('And so are these worksheets.')
 def twice(x):
     print(x, '*', x, 'is', x * x)
+def draw_face(x, y):
+    Circle((300, 200), 100)
+    Circle((265, 220), 15)
+    Circle((335, 220), 15)
+    Arc((300, 200), 60, 225, 315)
+    Line((300, 220), (280, 180))
+    Line((280, 180), (320, 180))
+    Circle((265, 220), 10)
+    Circle((335, 220), 10) 
