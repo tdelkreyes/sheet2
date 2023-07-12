755585042668fa332315d7cd62c1a2b168fd22ad
Travis <tdelkreyes@gmail.com>
2023-07-12 09:17:51 -0400

    added spaces between challenges

--- prettypictures.py
+++ prettypictures.py
@@ -8,11 +8,14 @@ Line((300, 220), (280, 180))
 Line((280, 180), (320, 180))
 Circle((265, 220), 10)
 Circle((335, 220), 10)
+
 def moan():
     print('Python is useless!')
     print('And so are these worksheets.')
+
 def twice(x):
     print(x, '*', x, 'is', x * x)
+
 def draw_face(x, y):
     Circle((300, 200), 150)
     Circle((265, 220), 15)
@@ -22,5 +25,6 @@ def draw_face(x, y):
     Line((280, 180), (320, 180))
     Circle((265, 220), 10)
     Circle((335, 220), 10) 
+
 update_when('key_pressed')
 end_graphics()
