46835aae4c5f0a5024c221c8d33cfb3ebb8a0f86
Travis <tdelkreyes@gmail.com>
2023-07-11 23:04:43 -0400

    face-drawing program into a face-drawing function

--- prettypictures.py
+++ prettypictures.py
@@ -1,16 +1,16 @@
 from gasp import *
 begin_graphics()
-Circle((300, 200), 100)
-Circle((265, 220), 15)
-Circle((335, 220), 15)
-Arc((300, 200), 60, 225, 315)
-Line((300, 220), (280, 180))
-Line((280, 180), (320, 180))
-Circle((265, 220), 10)
-Circle((335, 220), 10)
+def draw_face(x, y):
+    Circle((300, 200), 100)
+    Circle((265, 220), 15)
+    Circle((335, 220), 15)
+    Arc((300, 200), 60, 225, 315)
+    Line((300, 220), (280, 180))
+    Line((280, 180), (320, 180))
+    Circle((265, 220), 10)
+    Circle((335, 220), 10)
 def moan():
     print('Python is useless!')
     print('And so are these worksheets.')
 def twice(x):
     print(x, '*', x, 'is', x * x)
-    
