5c8b403da150ed3e1384c0690176f3c3a417fbc9
Travis <tdelkreyes@gmail.com>
2023-07-13 10:22:20 -0400

    display succes message

--- higher,lower.py
+++ higher,lower.py
@@ -1,2 +1,15 @@
 from random import randint
 
+number - randint(1, 6)
+print("Ok, I've thought of a number between 1 and 6.\n")
+prompt = "Make a guess: "
+answer = -1
+
+while answer != number:
+    answer = int(input(prompt))
+    if answer < number:
+        print("that's too low.\n")
+    elif answer > number:
+        print("that's too high.\n")
+
+print("That was my number. Well done!")
