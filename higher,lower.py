d6360ae9cadf8ba79ffbc6b31ab4413c349147a7
Travis <tdelkreyes@gmail.com>
2023-07-13 10:47:24 -0400

    while again =="yes":

--- higher,lower.py
+++ higher,lower.py
@@ -1,19 +1,21 @@
 from random import randint
 
-number = randint(1, 6)
-print("Ok, I've thought of a number between 1 and 6.\n")
-prompt = "Make a guess: "
-answer = -1
-counter = 0
+again = "yes"
 
-while answer != number:
-    answer = int(input(prompt))
-    counter = counter + 1
-    if answer < number:
-        print("that's too low.\n")
-    elif answer > number:
-        print("that's too high.\n")
+while again =="yes":
+    number = randint(1, 6)
+    print("Ok, I've thought of a number between 1 and 6.\n")
+    prompt = "Make a guess: "
+    answer = -1
+    counter = 0
 
-print("That was my number. Well done!")
-print(f'\nYou took {counter} guesses')
+    while answer != number:
+        answer = int(input(prompt))
+        counter = counter + 1
+        if answer < number:
+            print("that's too low.\n")
+        elif answer > number:
+            print("that's too high.\n")
 
+    print("That was my number. Well done!")
+    print(f'\nYou took {counter} guesses')
